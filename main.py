ab6842fe4d01704a6b83b97a1be4521c40aeeea1
Eriklp <lopez.erik@correounivalle.edu.co>
2018-04-05 14:23:06 -0500

    peueños cambios

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import pygame
+import pygame, sys
+from pygame.locals import *
 
-pygame.init()
-Screen = pygame.display.set_mode((1000, 1000))
-pygame.display.set_icon(pygame.image.load("Icon.png"))
-pygame.event.set_allowed(None)  # PyGame will now not check for any inputs
-allowed_events = (pygame.MOUSEMOTION, pygame.MOUSEBUTTONDOWN,
-                  pygame.KEYDOWN, pygame.KEYUP, pygame.QUIT)
-for event in allowed_events:         # PyGame will now only check inputs for things used in the
-    pygame.event.set_allowed(event)
+class Cursor(pygame.Rect):
+	def __init__(self):
+		pygame.Rect.__init__(self, 0,0,1,1)
+	def update(self):
+		self.left, self.top = pygame.mouse.get_pos()
+
+def main():
+	pygame.init()
+	ventana = pygame.display.set_mode((1000, 650))
+	#pygame.display.set_icon(pygame.image.load(""))
+	pygame.display.set_caption("Super Mario")
+
+	ima = pygame.image.load("Icon.png")
+	posX, posy = 600, 100
+
+	velocidad = 5
+	while True:
+
+		ventana.blit(ima, (posX, posy))
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+		pygame.display.update()
+
+main()
