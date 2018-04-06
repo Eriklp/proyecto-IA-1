f64cbccf861e6d11df813619f28b8a1529634a7b
Eriklp <lopez.erik@correounivalle.edu.co>
2018-04-06 18:11:20 -0500

    adelanto

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ class Cursor(pygame.Rect):
 	def update(self):
 		self.left, self.top = pygame.mouse.get_pos()
 
+class Boton(pygame.sprite.Sprite):
+	def __init__(self, ima1, ima2, x=800, y=20):
+		self.ima_normal=ima1
+		self.ima_seleccion=ima2
+		self.ima_actual=self.ima_normal
+		self.rect=self.ima_actual.get_rect()
+		self.rect.left, self.rect.top=(x, y)
+	def update(self, pantalla, cursor):
+		if cursor.colliderect(self.rect):
+			self.imagen_actual=self.ima_seleccion
+		else: self.ima_actual = self.ima_normal
+
+		pantalla.blit(self.ima_actual, self.rect)
+
+
 def main():
 	pygame.init()
 	ventana = pygame.display.set_mode((1000, 650))
@@ -17,6 +32,12 @@ def main():
 	posX, posy = 600, 100
 
 	velocidad = 5
+
+	imagenCarga = pygame.image.load("images/upload.png")
+
+	cursor1 = Cursor()
+	botonCarga = Boton(imagenCarga, imagenCarga)
+
 	while True:
 
 		ventana.blit(ima, (posX, posy))
